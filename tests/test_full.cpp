5ffb247d05f09cb49cf83f45ec2f9eeecdc507c3
Joris Bayer <joris.bayer@protonmail.com>
2020-04-04 22:55:49 +0200

    More full program test cases

--- tests/test_full.cpp
+++ tests/test_full.cpp
@@ -25,7 +25,7 @@ std::string eval(const std::string & code)
     const auto end = tokens.cend();
     const auto ast = parseScope(it, end, 0);
 
-    static Compiler compiler;
+    Compiler compiler;
     ast->acceptVisitor(compiler);
 
     std::stringstream stream;
@@ -39,7 +39,7 @@ std::string eval(const std::string & code)
 // TODO: why not part of "make test"?
 BOOST_AUTO_TEST_CASE(test_fibonacci)
 {
-    const auto code = R"__code__(
+    const auto code = R"###(
 f0 = 1
 f1 = 1
 
@@ -52,9 +52,25 @@ while i < 5
 
     i = i + 1
 print(f)
-)__code__";
+)###";
 
     BOOST_CHECK_EQUAL(eval(code), "13\n");
 }
 
+BOOST_AUTO_TEST_CASE(test_hello_world)
+{
+    BOOST_CHECK_EQUAL(eval("print(\"Hello, world!\")\n"), "Hello, world!\n");
+}
 
+
+BOOST_AUTO_TEST_CASE(void_function)
+{
+    const auto code = R"###(
+function greet(name: String)
+    print("Hello, " + name + "!")
+
+greet("Joris")
+)###";
+
+    BOOST_CHECK_EQUAL(eval(code), "Hello, Joris!\n");
+}
